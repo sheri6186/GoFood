0ddcf9f7227eafee7f265bc4030cc6e0f0c9ff19
sheri6186 <sheri.6186@gmail.com>
2024-08-13 12:51:47 +0500

    Structure is fixed

---
+++ mernapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './Screen/Home';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+   <div>
+    <div><Home/></div>
+    <div>body</div>
+    <div><Footer/></div>
+   </div>
+  );
+}
+
+export default App;
